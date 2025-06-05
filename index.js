ff81ae02fd9fcb310b59210e95a2af21a563b556
xiangxn <xiangxn@163.com>
2025-06-05 22:41:55 +0800

    add metamask

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import './polyfills.js';
 import BinanceW3WProvider, { getProvider } from '@binance/w3w-ethereum-provider';
+import { MetaMaskSDK } from '@metamask/sdk';
 
 window.BinanceW3WProvider = BinanceW3WProvider;
 window.getBinanceProvider = getProvider;
+window.MetaMaskSDK = MetaMaskSDK;
